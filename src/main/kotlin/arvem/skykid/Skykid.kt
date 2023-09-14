9105ae71cfbb6791798fc1a782f4006e63387709
arvemw <wyatt6of7@gmail.com>
2023-09-14 14:59:32 -0700

    Rewrote descriptions of some of the powers Added effect immunities

--- src/main/kotlin/arvem/skykid/Skykid.kt
+++ src/main/kotlin/arvem/skykid/Skykid.kt
@@ -3,18 +3,22 @@ package arvem.skykid
 import arvem.skykid.registries.RegisterItems
 import arvem.skykid.registries.RegisterSounds
 import net.fabricmc.api.ModInitializer
+import net.minecraft.registry.RegistryKeys.BLOCK
+import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
 
 object Skykid : ModInitializer {
 	const val MODID = "skykid"
 	const val MODNAME = "Sky Kid"
-
-    private val logger = LoggerFactory.getLogger(MODNAME)
+	val prime_sources = TagKey.of(BLOCK, Identifier(MODID, "prime_sources"))
+	private val logger = LoggerFactory.getLogger(MODNAME)
 
 	override fun onInitialize() {
-		logger.info("When kingdom come...")
 		RegisterItems.init()
 		RegisterSounds.init()
+		logger.info("When kingdom come...")
 	}
 }
+
